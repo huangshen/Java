1f49081f6f63a4a5039dc63623e902ad1a55f8eb
huangshen <huangshen29740@163.com>
2016-09-24 20:45:12 +0800

    Add more demos for chapater 'Initialization&Cleanup'
    Signed-off-by: huangshen <huangshen29740@163.com>

--- Initialization/src/InitializationDemo.java
+++ Initialization/src/InitializationDemo.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // Demonstration of constructor method
 class Rock {
 	static int nRocks = 0;
@@ -56,6 +58,65 @@ class Flower {
 	}
 }
 
+// Demonstration of finalize()
+class Book {
+	boolean checkedOut = false;
+	Book(boolean checkout) {
+		checkedOut = checkout;
+	}
+	void checkin() {
+		checkedOut = false;
+	}
+	protected void finalize() {
+		if(checkedOut == true) {
+			System.out.println("Error: checkout");
+		}
+	}
+}
+
+// Demonstration of initialization orders
+class Window {
+	Window(int num) {
+		System.out.println("Window " + num);
+	}
+}
+
+class House {
+	Window w1 = new Window(1);
+	House() {
+		System.out.println("House");
+		w3 = new Window(33);
+	}
+	Window w2 = new Window(2);
+	void f() {
+		System.out.println("f()");
+	}
+	Window w3 = new Window(3);
+}
+
+class Other {
+	public static void main(String[] args) {
+		for(String s : args) {
+			System.out.println(s);
+		}
+	}
+}
+
+// Creating variable argument list
+class NewVarArgs {
+	static void printArray(Object... args) {
+		for(Object obj : args) {
+			System.out.print(obj + "  ");
+		}
+		System.out.println();
+	} 
+}
+
+// Enum
+enum Spiciness {
+	NOT, MILD, MEDIUM, HOT, FLAMING
+}
+
 // Demonstration of charpter "Initialization & Cleanup"
 public class InitializationDemo {
 
@@ -75,6 +136,64 @@ public class InitializationDemo {
 		Flower f2 = new Flower("Red rose.");
 		Flower f3 = new Flower("White rose", 11);
 		Flower f4 = new Flower();
+		
+		new Book(true);
+		System.gc();
+		
+		House house = new House();
+		house.f();
+		
+		// Demo of array
+		int a1[] = {1, 2, 3, 4, 5};
+		int a2[];
+		a2 = a1;
+		
+		for(int i = 0; i < 5; i++) {
+			a2[i] = a2[i] + 1;
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(a1[i]);
+		}
+		
+		// Creating array with new
+		int[] a;
+		Random rand = new Random(48);
+		a = new int[rand.nextInt(10)];
+		
+		System.out.println("a.length = " + a.length);		
+
+		for(int i = 0; i < a.length; i++) {
+			a[i] = rand.nextInt(100);
+		}
+		
+		System.out.println(Arrays.toString(a));
+		
+		// Array initialization
+		Integer[] c = {
+				new Integer(1),
+				new Integer(2),
+				3,
+		};
+		
+		Integer[] d = new Integer[] {
+				new Integer(1),
+				new Integer(2),
+				3,
+		};
+		
+		System.out.println(Arrays.toString(c));
+		System.out.println(Arrays.toString(d));
+		
+		Other.main(new String[] {"aaa", "bbb", "ccc"});
+		
+		NewVarArgs.printArray(1, 2);
+		NewVarArgs.printArray("one", "two", "three");
+		NewVarArgs.printArray(new Window(11), new Window(12), new Window(13));
+		
+		for(Spiciness s : Spiciness.values()) {
+			System.out.println(s + ", odinal = " + s.ordinal() );
+		}
 	}
 
 }
